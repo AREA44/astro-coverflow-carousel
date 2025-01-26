3009142d6111ca933af066ec8eebf8cb6d05c613
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-01-27 01:33:54 +0700

    tailwindcss v4

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
+// @ts-check
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,8 +9,10 @@ export default defineConfig({
   base: 'astro-coverflow-carousel',
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
+  ],
+  vite: [
+    plugins: [
+      tailwindcss(),
+    ],
   ],
 })
